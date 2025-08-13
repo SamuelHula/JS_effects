696d1e7dbcfb1a7b27493e512c760019e0030abb
Samuel Hula <samko.hula@gmail.com>
2025-08-13 12:39:45 +0200

    adding typing effect

--- type/type.js
+++ type/type.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-   var typed = new Typed('.h1', {
-         strings: ["Samuel Hula", "a Web Developer", "a  Student"],
-         typeSpeed: 60,
-         loop: true,
-         backSpeed: 60
-   });
+      var typed = new Typed('.h1', {
+            strings: ["Samuel Hula", "a Web Developer", "a  Student"],
+            typeSpeed: 60,
+            loop: true,
+            backSpeed: 60
+      });
 });
